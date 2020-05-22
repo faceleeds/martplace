f1ad4687ccc3d4b845a80db402f59e964cbe728f
faceleeds <faceleeds@inbox.lv>
2020-05-22 04:27:38 +0300

    slider finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
-    
+    $(".rate-star").rateYo({
+        starWidth: "15px",
+        rating: 4.5,
+        readOnly: true,
+      });
+      
+      $('.slider__weekly-items').slick({
+        prevArrow:"<img class='a-right control-c next slick-prev' src='images/left-arrow.svg'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='images/right-arrow.svg'>",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // infinite: true,
+    arrows: true
+      });
 
 });
